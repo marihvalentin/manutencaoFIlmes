bd96e4fcf1a1602567392702eeba5563ca1385c5
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-08 16:36:01 -0300

    Subindo o projeto inicial gerado através do spring initializr

---
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/ManutencaoFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.test.mfilmes.manutencaoFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/test/mfilmes/manutencaoFilmes/ManutencaoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.test.mfilmes.manutencaoFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
