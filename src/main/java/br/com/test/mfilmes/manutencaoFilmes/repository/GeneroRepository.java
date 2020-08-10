66ecb9d8e48e854848a710947b6182838b4e1e09
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 18:26:16 -0300

    Adicionado o objeto o qual a JpaRepository estende e o tipo do id (Genero, Integer) e o método para busca do genero a partir do tipo, o que complementa o método converter presente na FilmeForm

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/GeneroRepository.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/GeneroRepository.java
@@ -1,8 +1,9 @@
 package br.com.test.mfilmes.manutencaoFilmes.repository;
 
+import br.com.test.mfilmes.manutencaoFilmes.model.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GeneroRepository extends JpaRepository
+public interface GeneroRepository extends JpaRepository<Genero, Integer>
 {
-
+    Genero findByTipo(String tipo);
 }
