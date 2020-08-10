1d8d66ba4a1e2d048ea685ea5fedb4e41dffd736
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 14:10:48 -0300

    Criado o método FindByTiulo no FilmeRepository

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/FilmeRepository.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/FilmeRepository.java
@@ -3,7 +3,10 @@ package br.com.test.mfilmes.manutencaoFilmes.repository;
 import br.com.test.mfilmes.manutencaoFilmes.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends JpaRepository<Filme, Integer>
 {
 
+    List<Filme> findByTitulo(String titulo);
 }
