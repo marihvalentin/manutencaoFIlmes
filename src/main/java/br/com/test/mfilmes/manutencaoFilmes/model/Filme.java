a32a3227b8fac1cc2d9c22c7148eb15cc6302055
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 12:17:38 -0300

    Adicionada a anotação @Entity sobre a declaração da classe, o que a torna entidade da JPA

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/model/Filme.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/model/Filme.java
@@ -1,9 +1,11 @@
 package br.com.test.mfilmes.manutencaoFilmes.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity(name = "filme") //transformando a classe em entidade da JPA
 public class Filme
 {
     @Id
