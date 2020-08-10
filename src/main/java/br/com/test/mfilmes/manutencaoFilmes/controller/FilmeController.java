17bdbbd20558485ca49fe307d8da9559c94c2011
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 13:45:17 -0300

    Injetado no FilmeController o repository recém criado: declarado o filmeRepository e no método listaFilme, foi apagada a criação de um filme na memória, a linha que criava um array.aslist e chamado o método findAll que consulta e carrega todos os registros do banco de dados.

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/FilmeController.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/FilmeController.java
@@ -2,35 +2,31 @@ package br.com.test.mfilmes.manutencaoFilmes.controller;
 
 import br.com.test.mfilmes.manutencaoFilmes.controller.dto.FilmeDto;
 import br.com.test.mfilmes.manutencaoFilmes.model.Filme;
+import br.com.test.mfilmes.manutencaoFilmes.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class FilmeController
 {
+    @Autowired
+    private FilmeRepository filmeRepository;
+
     @RequestMapping("/")
     public String index()
     {
         return "index";
     }
 
-    @RequestMapping("/listarFilmes.html")
+    @RequestMapping("/listarFilmes")
     @ResponseBody
     public List<FilmeDto> listaFilmes()
     {
-        Filme filme = new Filme("Marvel's The Avengers", "Joss Whedon",
-                "Ação, Ficção Científica", "Loki, o irmão de Thor, ganha acesso ao poder ilimitado " +
-                "do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta " +
-                "agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de " +
-                "defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra " +
-                "e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de " +
-                "lado e agir como um grupo em prol da humanidade.", 2012);
-
-        return FilmeDto.converter(Arrays.asList(filme, filme, filme));
+        List<Filme> filmes = filmeRepository.findAll(); //consulta e traz todos os registros do banco de dados
+        return FilmeDto.converter(filmes);
     }
 
 }
