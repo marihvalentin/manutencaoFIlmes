0a869d5d1a592f7d1bb29afbaaebab7a396578fb
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 18:18:07 -0300

    Adicionada a classe FilmeForm, representando os dados que chegarão a API a partir do cliente, para referenciar ao cadastro/inclusão de filmes

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/form/FilmeForm.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/form/FilmeForm.java
@@ -1,4 +1,62 @@
 package br.com.test.mfilmes.manutencaoFilmes.controller.form;
 
-public class FilmeForm {
+import br.com.test.mfilmes.manutencaoFilmes.model.Filme;
+import br.com.test.mfilmes.manutencaoFilmes.model.Genero;
+import br.com.test.mfilmes.manutencaoFilmes.repository.GeneroRepository;
+
+public class FilmeForm
+{
+    private String titulo;
+    private String diretor;
+    private String tipoGenero;
+    private String sinopse;
+    private int ano;
+
+    //getters e setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public String getTipoGenero() {
+        return tipoGenero;
+    }
+
+    public void setTipoGenero(String tipoGenero) {
+        this.tipoGenero = tipoGenero;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    //metodo que converte
+    public Filme converter(GeneroRepository generoRepository)
+    {
+        Genero genero = generoRepository.findByTipo(tipoGenero);
+        return new Filme(titulo, diretor, genero, sinopse, ano);
+    }
 }
