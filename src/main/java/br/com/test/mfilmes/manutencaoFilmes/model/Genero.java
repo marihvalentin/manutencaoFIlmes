7c9060865ead544d28b22ee8840e6c10136b6ae4
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 17:09:48 -0300

    Declaração dos atributos relacionados aos gêneros de filmes e gerados os getters e setters

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/model/Genero.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/model/Genero.java
@@ -2,5 +2,27 @@ package br.com.test.mfilmes.manutencaoFilmes.model;
 
 public class Genero
 {
+    private Integer id;
+    private String tipo;
 
+    //getters e setters
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getTipo()
+    {
+        return tipo;
+    }
+
+    public void setTipo(String tipo)
+    {
+        this.tipo = tipo;
+    }
 }
---
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/GeneroRepository.java
@@ -0,0 +1,4 @@
+package br.com.test.mfilmes.manutencaoFilmes.repository;
+
+public interface GeneroRepository {
+}
