87d6c666492c705aef1753d9eaf7e081c3c47141
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 18:20:13 -0300

    Adicionada anotação para listar filmes a partir do filtro de gênero

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/FilmeRepository.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/repository/FilmeRepository.java
@@ -2,11 +2,11 @@ package br.com.test.mfilmes.manutencaoFilmes.repository;
 
 import br.com.test.mfilmes.manutencaoFilmes.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface FilmeRepository extends JpaRepository<Filme, Integer>
 {
-
     List<Filme> findByTitulo(String titulo);
+
+    List<Filme> findByGenero(String genero);
 }
