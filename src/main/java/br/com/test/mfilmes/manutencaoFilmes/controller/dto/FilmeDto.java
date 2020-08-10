212ad803083b21e101118a48b46270b507ffb3f4
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-10 11:51:56 -0300

    Adicionada classe FilmeDto, padrão estabelecido para a listagem apenas de alguns dados específicos dos filmes, título e diretor, os quais foram atribuídos ao método de listagem de filmes presente na FilmeController.

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/dto/FilmeDto.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/dto/FilmeDto.java
@@ -1,4 +1,34 @@
 package br.com.test.mfilmes.manutencaoFilmes.controller.dto;
 
-public class FilmeDto {
+import br.com.test.mfilmes.manutencaoFilmes.model.Filme;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilmeDto
+{
+    private String titulo;
+    private String diretor;
+
+    public FilmeDto(Filme filme)
+    {
+        this.titulo = filme.getTitulo();
+        this.diretor = filme.getDiretor();
+    }
+
+    //Converter uma lista de Filme para uma lista de FilmeDto com o Java8
+    public static List<FilmeDto> converter(List<Filme> filmes)
+    {
+        return filmes.stream().map(FilmeDto::new).collect(Collectors.toList());
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public String getDiretor()
+    {
+        return diretor;
+    }
 }
